c1fbaae7126b3b90d002a7f79a1b7f2903369fff
Supzero-0 <mat.lecanu@gmail.com>
2022-12-02 10:33:57 +0100

    quete 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,25 +4,38 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/program', name: 'program_')]
+
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render(
             'program/index.html.twig',
-            ['website' => 'Wild Series',]
+            ['programs' => $programs]
         );
     }
 
     #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
-    public function show(int $id): Response
+
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', ['program' => $program,]);
     }
 }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -13,6 +13,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         ['title' => 'Walking dead', 'synopsis' => 'Des zombies envahissent la terre', 'category' => 'category_Action'],
         ['title' => 'Mercredi', 'synopsis' => 'La famille Adams de retour', 'category' => 'category_Fantastique'],
         ['title' => 'Cyberpunk Edgerunner', 'synopsis' => 'Des humains modifiés se battent pour survivre', 'category' => 'category_Animation'],
+        ['title' => 'Rick et Morty', 'synopsis' => 'Un grand-père savant fou avec sa famille loufoque', 'category' => 'category_Animation'],
+        ['title' => 'JoJo\'s Bizarre Adeventure', 'synopsis' => 'Muda muda muda muda muda', 'category' => 'category_Animation'],
         ['title' => 'The 100', 'synopsis' => 'Des humains survivent sur une Terre dévasté par la pollution', 'category' => 'category_Aventure'],
         ['title' => 'Resident Evil', 'synopsis' => 'Un virus tueur, l\'humanité en péril', 'category' => 'category_Horreur'],
     ];

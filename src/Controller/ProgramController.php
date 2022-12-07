9bd0c04fcd54ea79008dd9be6f56be93c1c2a85c
Supzero-0 <mat.lecanu@gmail.com>
2022-12-07 14:18:38 +0100

    quete 11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,6 +5,7 @@
 namespace App\Controller;
 
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,12 +31,30 @@ class ProgramController extends AbstractController
     public function show(int $id, ProgramRepository $programRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $id]);
+        $seasons = $program->getSeasons();
 
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with id : ' . $id . ' found in program\'s table.'
             );
         }
-        return $this->render('program/show.html.twig', ['program' => $program,]);
+        return $this->render('program/show.html.twig', ['program' => $program, 'seasons' => $seasons]);
+    }
+
+    #[Route('/{programId}/seasons/{seasonId}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'season_show')]
+
+    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $programId]);
+        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+        $episodes = $season->getEpisodes();
+
+        if (!$episodes) {
+            throw $this->createNotFoundException(
+                'No episodes for this program !'
+            );
+        }
+
+        return $this->render('season/show.html.twig', ['program' => $program, 'season' => $season, 'episodes' => $episodes]);
     }
 }
